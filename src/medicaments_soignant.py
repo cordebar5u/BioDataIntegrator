e0790859b641bbfa6723a098e0b9fc97b149aa4e
CORDEBAR Benjamin <benjamin.cordebar@telecomnancy.eu>
2024-03-29 11:19:59 +0100

    Fonctionnalité - modification médicament_soigant.py

--- src/medicaments_soignant.py
+++ src/medicaments_soignant.py
@@ -11,19 +11,32 @@ def medicaments_soignant_symptomes(maladies):
         medicaments (list): liste des médicaments soignant
 
     '''
+
+    exist_medicaments = False
+ 
+    retour= []   # liste avec le chemin, les maladies et les médicaments
     medicaments = []
 
     fichier_xml = "data/DRUGBANK/drugbank_modifiee.xml"
 
-    medicaments.extend(cdb.rechercher_medicament(fichier_xml, maladies[0][1], ""))  # Recherche les médicaments soignant les maladies
-    medicaments = list(set(medicaments))  # Supprime les doublons
-    
-    if medicaments == []:
+    for maladie in maladies:
+        medicaments.extend(cdb.rechercher_medicament(fichier_xml, maladie[1], "")) # Recherche les médicaments soignant les maladies
+        medicaments = list(set(medicaments))  # Supprime les doublons
+
+        retour.append([maladie[0], maladie[1], medicaments])
+
+        if medicaments != [] and not exist_medicaments:
+            exist_medicaments = True
+        
+        medicaments = [] # Réinitialise la liste des médicaments
+
+
+    if not exist_medicaments:
         print("Aucun médicament trouvé soignant l'indication/symptome.")
 
     else:
         print("\n\n\nLes médicaments soignant les maladies sont : ")
-        for i in medicaments:
-            print(i)
+        for i in retour :
+            print(i[2])
     print("\n\n\n")
-    return medicaments
+    return retour
